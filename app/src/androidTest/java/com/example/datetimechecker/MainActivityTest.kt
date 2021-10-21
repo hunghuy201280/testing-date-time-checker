025e9a252b765c2baf260233a434f17a970313a5
hunghuy201280 <humghuy201280@gmail.com>
2021-10-21 21:19:36 +0700

    update unit test

--- app/src/androidTest/java/com/example/datetimechecker/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/datetimechecker/MainActivityTest.kt
@@ -5,6 +5,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MainActivityTest {
@@ -149,5 +150,216 @@ class MainActivityTest {
 
     }
 
+    @Test
+    fun testFunction(){
+        val scenario = ActivityScenario.launch(MainActivity::class.java);
+        var day="1";
+        var month="6";
+        var year="2000";
+        //region 1/6/2000
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        //region 2/6/2000
+        day="2";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 15/6/2000
+        day="15";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 30/6/2000
+        day="30";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 31/6/2000
+        day="31";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //false
+        onView(withText("$day/$month/$year is NOT correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+
+        // region 15/1/2000
+        day="15";
+        month="1";
+        year="2000";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+
+        // region 15/2/2000
+        day="15";
+        month="2";
+        year="2000";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+    // region 15/11/2000
+        day="15";
+        month="11";
+        year="2000";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+
+        // region 15/12/2000
+        day="15";
+        month="12";
+        year="2000";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 15/6/1801
+        day="15";
+        month="6";
+        year="1801";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+
+        // region 15/6/1802
+        day="15";
+        month="6";
+        year="1802";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 15/6/2998
+        day="15";
+        month="6";
+        year="1801";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+        // region 15/6/2999
+        day="15";
+        month="6";
+        year="2999";
+        onView(withId(R.id.dayEditText)).perform(typeText(day));
+        onView(withId(R.id.monthEditText)).perform(typeText(month));
+        onView(withId(R.id.yearEditText)).perform(typeText(year));
+        onView(withId(R.id.checkButton)).perform(click());
+        //true
+        onView(withText("$day/$month/$year is correct date time!")).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(pressBack());
+        onView(withId(R.id.clearButton)).perform(click());
+
+        //endregion
+
+
+//        assertEquals(true,  mainActivity.isValidDate(2998,6,15))
+//        assertEquals(true,  mainActivity.isValidDate(2999,6,15))
+    }
 
 }
